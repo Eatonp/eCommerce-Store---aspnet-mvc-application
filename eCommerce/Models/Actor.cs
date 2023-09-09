cc06044dfd726817d169e5657cc4bc27ccd28d8c
eatonp <paul.eaton@zeninternet.co.uk>
2023-09-09 20:42:56 +0100

    latest version

--- eCommerce/Models/Actor.cs
+++ eCommerce/Models/Actor.cs
@@ -8,8 +8,11 @@ namespace eCommerce.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name ="Profile Picure")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
 
